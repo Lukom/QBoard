98ecb9ae4b09c6fa36ba2f43a3a0bac8055598ad
Lev Lukomsky <lllukom@gmail.com>
2010-08-21 15:14:29 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qboard::Application.config.secret_token = '46c76ca2df6d520b1f054afcfd2bcedda730f0652f8434f7c294f52adf7608830d6ffeff67706d0e74fb797fff1b8b0b00b1f4415f00c0eddf56867eaa7f21e4'
